7be9617fe1e230ad269db64239625e3133ee9454
Sumet Chorthongdee <sumetchor@gmail.com>
2020-08-29 17:11:47 +0700

    initial commit

---
+++ client.js
@@ -0,0 +1,54 @@
+// CLIENT
+
+const axios = require('axios');
+
+function listBooks() {
+    axios.get('http://localhost:10000/books')
+        .then((response) => {console.log(response.data); return;})
+        .catch((error) => {console.log(error); return;})
+}
+
+function insertBook(id, title, author) {
+    const book = {
+        id: parseInt(id),
+        title: title,
+        author: author
+    };
+    axios.post('http://localhost:10000/books', book)
+        .then((response) => {console.log(response.data); return;})
+        .catch((error) => {console.log(error); return;})
+}
+
+function getBook(id) {
+    axios.get(`http://localhost:10000/books/${id}`, book)
+        .then((response) => {console.log(response.data); return;})
+        .catch((error) => {console.log(error); return;})
+}
+
+function deleteBook(id) {
+    axios.delete(`http://localhost:10000/books/${id}`)
+        .then((response) => {console.log(response.data); return;})
+        .catch((error) => {console.log(error); return;})
+}
+
+function watchBooks() {
+    var call = client.watch({});
+    call.on('data', function(book) {
+        console.log(book);
+    });
+}
+
+var processName = process.argv.shift();
+var scriptName = process.argv.shift();
+var command = process.argv.shift();
+
+if (command == 'list')
+    listBooks();
+else if (command == 'insert')
+    insertBook(process.argv[0], process.argv[1], process.argv[2]);
+else if (command == 'get')
+    getBook(process.argv[0]);
+else if (command == 'delete')
+    deleteBook(process.argv[0]);
+else if (command == 'watch')
+    watchBooks();
